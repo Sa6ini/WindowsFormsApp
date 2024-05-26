e65d658577d500d2f44ea6e21e014150331675b8
Sa6ini <76049482+Sa6ini@users.noreply.github.com>
2024-05-26 23:35:59 +0300

    Add files via upload

---
+++ Products.Designer.cs
@@ -0,0 +1,165 @@
+﻿namespace Vetov_2._0
+{
+    partial class Products
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            button1 = new Button();
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            comboBox1 = new ComboBox();
+            button2 = new Button();
+            button3 = new Button();
+            numbers = new TextBox();
+            SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.Cursor = Cursors.Hand;
+            button1.FlatAppearance.BorderSize = 0;
+            button1.FlatStyle = FlatStyle.Flat;
+            button1.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            button1.ForeColor = Color.Black;
+            button1.Location = new Point(421, -2);
+            button1.Name = "button1";
+            button1.Size = new Size(35, 47);
+            button1.TabIndex = 0;
+            button1.Text = "X";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 18F, FontStyle.Bold, GraphicsUnit.Point);
+            label1.Location = new Point(146, 79);
+            label1.Name = "label1";
+            label1.Size = new Size(195, 32);
+            label1.TabIndex = 1;
+            label1.Text = "Choose Product";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI", 11.25F, FontStyle.Bold, GraphicsUnit.Point);
+            label2.Location = new Point(56, 168);
+            label2.Name = "label2";
+            label2.Size = new Size(193, 20);
+            label2.TabIndex = 2;
+            label2.Text = "Choose Nuber of Products";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            label3.Location = new Point(56, 217);
+            label3.Name = "label3";
+            label3.Size = new Size(130, 21);
+            label3.TabIndex = 3;
+            label3.Text = "Choose Product";
+            // 
+            // comboBox1
+            // 
+            comboBox1.FormattingEnabled = true;
+            comboBox1.Items.AddRange(new object[] { "Tomatoes 5.49$", "Carrots 7.47$", "Cucumbers 2.47$", "Bananas 1.78$" });
+            comboBox1.Location = new Point(219, 215);
+            comboBox1.Name = "comboBox1";
+            comboBox1.Size = new Size(154, 23);
+            comboBox1.TabIndex = 5;
+            // 
+            // button2
+            // 
+            button2.BackColor = Color.DodgerBlue;
+            button2.FlatAppearance.BorderSize = 0;
+            button2.FlatStyle = FlatStyle.Flat;
+            button2.Font = new Font("Segoe UI", 15.75F, FontStyle.Bold, GraphicsUnit.Point);
+            button2.ForeColor = Color.GhostWhite;
+            button2.Location = new Point(199, 282);
+            button2.Name = "button2";
+            button2.Size = new Size(205, 46);
+            button2.TabIndex = 6;
+            button2.Text = "ADD PRODUCT";
+            button2.UseVisualStyleBackColor = false;
+            button2.Click += button2_Click;
+            // 
+            // button3
+            // 
+            button3.BackColor = Color.DodgerBlue;
+            button3.FlatAppearance.BorderSize = 0;
+            button3.FlatStyle = FlatStyle.Flat;
+            button3.Font = new Font("Segoe UI", 18F, FontStyle.Bold, GraphicsUnit.Point);
+            button3.ForeColor = Color.White;
+            button3.Location = new Point(105, 415);
+            button3.Name = "button3";
+            button3.Size = new Size(281, 58);
+            button3.TabIndex = 7;
+            button3.Text = "GENERATE FINAL";
+            button3.UseVisualStyleBackColor = false;
+            button3.Click += button3_Click;
+            // 
+            // numbers
+            // 
+            numbers.Location = new Point(273, 165);
+            numbers.Name = "numbers";
+            numbers.Size = new Size(100, 23);
+            numbers.TabIndex = 9;
+            // 
+            // Products
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(454, 545);
+            Controls.Add(numbers);
+            Controls.Add(button3);
+            Controls.Add(button2);
+            Controls.Add(comboBox1);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(button1);
+            ForeColor = Color.DodgerBlue;
+            FormBorderStyle = FormBorderStyle.None;
+            Name = "Products";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "Form3";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button button1;
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private ComboBox comboBox1;
+        private Button button2;
+        private Button button3;
+        private TextBox numbers;
+    }
+}
---
+++ Products.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Vetov_2._0
+{
+    public partial class Products : Form
+    {
+        public Products()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+
+
+
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+            var products = comboBox1.SelectedItem.ToString();
+            var infoProduct = products
+                              .Split(" ",
+                               StringSplitOptions.RemoveEmptyEntries);
+            string nameofproduct = infoProduct[0];
+            double price = double.Parse(infoProduct[1].TrimEnd('$'));
+
+            int num = int.Parse(numbers.Text);
+            if (num <= 0)
+            {
+                MessageBox.Show("Number of products cannot be 0 or less", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+            if (comboBox1.SelectedItem == null)
+            {
+                MessageBox.Show("Select a product", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            double total = 0;
+        }
+    }
+}
